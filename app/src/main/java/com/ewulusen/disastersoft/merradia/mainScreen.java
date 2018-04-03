f067554f5f9609453e0461b14b19048a81d7a0f0
Zoltán Diszterhöft <Diszterhoft.Zoltan@KE.LOCAL>
2018-04-03 15:43:41 +0200

    make charakter
    in progress

--- app/src/main/java/com/ewulusen/disastersoft/merradia/mainScreen.java
+++ app/src/main/java/com/ewulusen/disastersoft/merradia/mainScreen.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.database.Cursor;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class mainScreen extends AppCompatActivity {
@@ -11,6 +13,7 @@ public class mainScreen extends AppCompatActivity {
     public static String id;
     databaseHelper userDB;
     TextView kiir;
+    Button mkchar,editChar,fight;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,6 +32,18 @@ public class mainScreen extends AppCompatActivity {
             findViewById(R.id.fight).setEnabled(false);
             findViewById(R.id.editChar).setEnabled(false);
         }
+        mkchar=findViewById(R.id.makechar);
+        mkchar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent2 = null;
+                intent2 = new Intent(mainScreen.this, MakeCharakter.class);
+                intent2.putExtra("datas", id);
+                startActivity(intent2);
+                finish();
+            }
+        });
+
 
     }
 }
