7df8bf9158d287c475e1d222445b7f6df982729b
Zoltán Diszterhöft <Diszterhoft.Zoltan@KE.LOCAL>
2018-04-03 14:34:50 +0200

    registration+login
    Regisztráció és login rendben, később lehet hozzá tenni egyébb ellenőrző
    funkciókat, de nem lényeges.
    bejelentkezéskor ellenőrzi van e kész karaktered.

--- app/src/main/java/com/ewulusen/disastersoft/merradia/databaseHelper.java
+++ app/src/main/java/com/ewulusen/disastersoft/merradia/databaseHelper.java
@@ -22,9 +22,9 @@ public class databaseHelper
     public static final String DatabaseName = "merradiaUsers.db";
     public static final String Username = "userName";
     public static final String Password = "password";
-    public static final String uTableName = "users_table";
+    public static final String uTableName = "users_table_Merradia";
     public static final String owner = "OWNER";
-    public static final String charTable = "unit_table";
+    public static final String charTable = "unit_table_Merradia";
     public databaseHelper(Context paramContext)
     {
         super(paramContext, "merradiaUsers.db", null, 12);
@@ -38,7 +38,7 @@ public class databaseHelper
     public  Cursor getName(String id)
     {
     SQLiteDatabase localSQLiteDatabase = getReadableDatabase();
-    String str1 = "SELECT userName FROM users_table where ID='"+id+"'";
+    String str1 = "SELECT userName FROM users_table_Merradia where ID='"+id+"'";
     Log.d("SQL", str1);
     Cursor localCursor = localSQLiteDatabase.rawQuery(str1, null);
     return localCursor;
@@ -51,7 +51,7 @@ public class databaseHelper
     public Cursor getChar(String id)
     {
         SQLiteDatabase localSQLiteDatabase = getReadableDatabase();
-        String str1 = "SELECT * FROM unit_table where OWNER='"+id+"'";
+        String str1 = "SELECT * FROM unit_table_Merradia where OWNER='"+id+"'";
         Log.d("SQL", str1);
         Cursor localCursor = localSQLiteDatabase.rawQuery(str1, null);
         return localCursor;
@@ -61,7 +61,7 @@ public class databaseHelper
      * hozzá ad egy sort a felhasználó táblához
      * @param paramString1-email
      * @param paramString2-password
-     * @return
+     * @return boolen
      */
     public boolean addData(String paramString1, String paramString2)
     {
@@ -69,7 +69,7 @@ public class databaseHelper
         ContentValues localContentValues = new ContentValues();
         localContentValues.put("userName", paramString1);
         localContentValues.put("password", paramString2);
-        return localSQLiteDatabase.insert("users_table", null, localContentValues) != -1L;
+        return localSQLiteDatabase.insert("users_table_Merradia", null, localContentValues) != -1L;
     }
 
     /**
@@ -79,26 +79,26 @@ public class databaseHelper
      */
     public Cursor backLogin(String paramString)
     {
-        return getReadableDatabase().rawQuery("SELECT * FROM users_table where ID=" + paramString + "", null);
+        return getReadableDatabase().rawQuery("SELECT * FROM users_table_Merradia where ID=" + paramString + "", null);
     }
 
     /**
      * Bejelentkezés/vagy regisztrál egy felhasználót
      * @param paramString1-email
      * @param paramString2-jelszó
-     * @return
+     * @return Cursor
      */
     public Cursor login(String paramString1, String paramString2)
     {
         SQLiteDatabase localSQLiteDatabase = getReadableDatabase();
         Log.d("SQL", paramString2);
-        String str1 = "SELECT * FROM users_table where userName='"+paramString1+"' and  password='" + paramString2 + "'";
+        String str1 = "SELECT * FROM users_table_Merradia where userName='"+paramString1+"' and  password='" + paramString2 + "'";
         Log.d("SQL", str1);
         Cursor localCursor = localSQLiteDatabase.rawQuery(str1, null);
         if (localCursor.getCount() == 0)
         {
             addData(paramString1, paramString2);
-            String str2 = "SELECT * FROM users_table where userName='"+paramString1+"' and password='" + paramString2 + "'";
+            String str2 = "SELECT * FROM users_table_Merradia where userName='"+paramString1+"' and password='" + paramString2 + "'";
             //Log.d("SQL", str2);
             localCursor = localSQLiteDatabase.rawQuery(str2, null);
         }
@@ -107,15 +107,15 @@ public class databaseHelper
     }
     public void onCreate(SQLiteDatabase paramSQLiteDatabase)
     {
-        paramSQLiteDatabase.execSQL("CREATE TABLE IF NOT EXISTS users_table (ID INTEGER PRIMARY KEY AUTOINCREMENT,  userName TEXT, password TEXT)");
-        paramSQLiteDatabase.execSQL("CREATE TABLE IF NOT EXISTS unit_table( ID INTEGER PRIMARY KEY AUTOINCREMENT,   OWNER TEXT ,Name TEXT,AGI TEXT,STR TEXT,DEF TEXT,CON TEXT,REF TEXT,LUCK TEXT)");
+        paramSQLiteDatabase.execSQL("CREATE TABLE IF NOT EXISTS users_table_Merradia (ID INTEGER PRIMARY KEY AUTOINCREMENT,  userName TEXT, password TEXT)");
+        paramSQLiteDatabase.execSQL("CREATE TABLE IF NOT EXISTS unit_table_Merradia( ID INTEGER PRIMARY KEY AUTOINCREMENT,   OWNER TEXT, Name TEXT,  AGI TEXT,   STR TEXT,   DEF TEXT,   CON TEXT,   REF TEXT,   LUCK TEXT)");
     }
 
     public void onUpgrade(SQLiteDatabase paramSQLiteDatabase, int paramInt1, int paramInt2)
     {
-        paramSQLiteDatabase.execSQL("DROP TABLE if EXISTS users_table");
+        paramSQLiteDatabase.execSQL("DROP TABLE if EXISTS users_table_Merradia");
         onCreate(paramSQLiteDatabase);
-        paramSQLiteDatabase.execSQL("DROP TABLE if EXISTS unit_table");
+        paramSQLiteDatabase.execSQL("DROP TABLE if EXISTS unit_table_Merradia");
         onCreate(paramSQLiteDatabase);
     }
 }
